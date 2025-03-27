056eeb1663c031eeecd2a590f241f689d2d65a19
İrem Bengü Bal <116900280+bengubal@users.noreply.github.com>
2025-03-27 13:33:41 +0300

    Proje başlangıç dosyası
    Spring proje dosyası yüklendi.

---
+++ bolumYonetimSistemi/src/main/java/com/project/bolumYonetimSistemi/BolumYonetimSistemiApplication.java
@@ -0,0 +1,13 @@
+package com.project.bolumYonetimSistemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolumYonetimSistemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolumYonetimSistemiApplication.class, args);
+	}
+
+}
---
+++ bolumYonetimSistemi/src/test/java/com/project/bolumYonetimSistemi/BolumYonetimSistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bolumYonetimSistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolumYonetimSistemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
